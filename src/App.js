c2a27deb0111929abfa68bbcaea0042ead63a0bf
Saikiran Patil <patilrsaikiran@gmail.com>
2022-10-23 21:06:01 +0530

    corrected page to load home if no other route is not found

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 import Events from './components/Events/Events'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Social from './components/Social/Social';
 
 function App() {
 
   return (
-    
+
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/events" element={<Events />} />
         <Route path="/social" element={<Social />} />
-        <Route path="*" element={<Social />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
     </BrowserRouter>
