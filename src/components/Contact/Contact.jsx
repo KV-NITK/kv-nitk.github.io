5be2e2834585ab20b475e45c9410c6a494c6576e
Chikkeri Chinmaya <121938335+chinmay1724@users.noreply.github.com>
2024-09-18 13:00:51 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -14,22 +14,22 @@ const Contact = () => {
                     <div className="section">
                         <div className="row">
                             <div className="footer-col col-4 contact-card">
-                                <h3>Raviraj H.M</h3>
+                                <h3>Dr.Kiran M </h3>
                                 <div>Faculty Advisor</div>
-                                <div>+91 9448861733</div>
-                                <div>ravirajmh@nitk.edu.in</div>
+                                <div>+91 8242473561 </div>
+                                <div>kiranmanjappa@nitk.edu.in</div>
                             </div>
                             <div className="footer-col col-4 contact-card">
-                                <h3>Arun Hasabi</h3>
+                                <h3>Sumukh S K</h3>
                                 <div>Convenor</div>
-                                <div>+91 6362679623</div>
-                                <div>arunhasabi.201me308@nitk.edu.in</div>
+                                <div>+91 94812 46683</div>
+                                <div>sksumukha.211ee153@nitk.edu.in</div>
                             </div>
                             <div className="footer-col col-4 contact-card">
-                                <h3>Chinmay Kadole</h3>
+                                <h3>Gururaj Mahadev Madannavar</h3>
                                 <div>Marketing Head</div>
-                                <div>+91 9986130382</div>
-                                <div>chinpgkadole.201me315@nitk.edu.in</div>
+                                <div>+91 8310326367</div>
+                                <div>gururajmahadevmadannavar.211ec115@nitk.edu.in</div>
                             </div>
                         </div>
                     </div>
